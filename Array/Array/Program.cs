a2dfb1a2c74fee872b324aa386d9bb8d0d870e63
Noman <noman199826@gmail.com>
2021-08-26 12:53:15 +0600

    Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+
+{
+    class Array
+    {
+         
+
+            static void Main(string[] args)
+            {
+                
+                 
+                Console.Write("Enterv size of an  array: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[size];
+             
+                Console.WriteLine("Enter {0} elements in the array", size);
+                for (int i = 0; i < size; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                
+                Console.Write("\nElements in array are : ");
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write("{0} ", arr[i]);
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+
+
+
+}
+
