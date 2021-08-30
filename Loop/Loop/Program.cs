3124a129d0f798fca48d4ed30a133e3850c74c7b
Noman <noman199826@gmail.com>
2021-08-30 23:09:15 +0600

    basic loop concept

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Data Type
+            //Data Type 2 Types (User defiined and predefined)
+            //predefined int(32) decimal(12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            //whileLoop
+            int i = 0;
+            while (i<30)
+            {
+                i++;
+
+                if (i%2==0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+                i++;
+
+            }*/
+         
+
+
+            /*
+            //for Loop
+
+            for( int i = 0; i<10; i++)
+            {
+
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+           
+              
+
+            }
+                */
+
+
+
+
+            Console.ReadKey();
+
+          
+        
+           
+          
+
+
+
+
+        }
+    }
+}
