f49ac7aabb10c726e2657b82e2deef727f7d9fcf
Noman <noman199826@gmail.com>
2021-08-31 10:39:04 +0600

    basic method

---
+++ Method/Method/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int result =  Add(3, 4);
+            Console.WriteLine(result);
+             
+            int sumresult = Sub(5, 3);
+            Console.WriteLine(sumresult);
+            Console.ReadKey();
+
+        }
+        static int Add (int firstNum, int secondNum)
+        {
+
+            int sum = firstNum + secondNum;
+            return sum;
+
+        }
+        static int Sub(int firstNum, int secondNum)
+        {
+
+            int sub = firstNum - secondNum;
+            return sub;
+
+        }
+
+
+
+
+    }
+}
