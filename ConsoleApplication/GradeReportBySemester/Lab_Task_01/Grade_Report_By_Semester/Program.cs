547b974672508327f1491abc5428f0b4b40d937e
Noman <noman199826@gmail.com>
2021-09-22 17:27:10 +0600

    GradeReportBySemester

---
+++ ConsoleApplication/GradeReportBySemester/Lab_Task_01/Grade_Report_By_Semester/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Report_By_Semester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name                : Najmul Uddin (Noman)");
+            Console.WriteLine("Id                  : 18-38293-2  ");
+            Console.WriteLine("Credit(s) Completed : 115 ");
+            Console.WriteLine("Course(s) Completed : 42  ");
+            Console.WriteLine("Cgpa                : 3.84 ");
+            Console.WriteLine("Core                : BScCSE, Summer 17-18, Core  ");
+            Console.WriteLine("Major               : BScCSE, Summer 17-18, Elective ");
+            Console.WriteLine(" Second Major       : Not Declared Yet ");
+            Console.WriteLine(" Elective           :  ");
+
+            Console.WriteLine("           Grade Report By Semester                                 ");
+
+            Console.WriteLine("*** 2021-2022, Fall                                           ");
+            Console.WriteLine(" Course Id           Course Name                                Course Credit ");
+            Console.WriteLine(" 00982	            ADVANCE DATABASE MANAGEMENT SYSTEM	       3.00	 ");
+            Console.WriteLine(" 01440	            OBJECT ORIENTED PROGRAMMING 2	           3.00	 ");
+            Console.WriteLine(" 01455	            OPERATING SYSTEM	                       3.00	 ");
+            Console.WriteLine(" 01473               RESEARCH METHODOLOGY	                   3.00  ");
+            Console.WriteLine(" 01559           	SOFTWARE REQUIREMENT ENGINEERING	       3.00	 ");
+            Console.ReadKey();   
+        }
+    }
+}
